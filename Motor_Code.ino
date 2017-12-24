eb014a5e5b1d9b77210d617b8c9e0d8fee2522c2
gilbertcasey2 <gilbertcasey2@gmail.com>
2017-12-24 14:03:38 -0500

    Commented the motor code a bit better.

--- Motor_Code.ino
+++ Motor_Code.ino
@@ -1,4 +1,19 @@
+///
+// Author: Casey E Gilbert
+// Name: Motor_Code
+// Purpose: This code is installed on all of the Arduinos that
+// 	    control motors for the SmilesPErHour installation.
+//	    The arduino running this code controlls two motors 
+//          at a time. It recieves data from the processing code,
+//          uses it to calculate how far it needs to move its
+//          motor, and then turns the motor (which in turn lowers
+//          the balls up and down).
+/// 
+
+// the library used to control the motor
 #include <AccelStepper.h>
+
+// define our macros
 #define HALFSTEP 8
 #define ROTATE 4096
 #define INCH 435
@@ -15,8 +30,8 @@
 #define motorPin24  11     // IN4 on the ULN2003 driver 2
 
 // Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
-AccelStepper stepper1(HALFSTEP, motorPin1, motorPin3, motorPin2, motorPin4);
-AccelStepper stepper2(HALFSTEP, motorPin21, motorPin23, motorPin22, motorPin24);
+AccelStepper stepper1(HALFSTEP, motorPin1, motorPin3, motorPin2, motorPin4);  // the first motor
+AccelStepper stepper2(HALFSTEP, motorPin21, motorPin23, motorPin22, motorPin24); // the second motor
 
 int maxSpeed = 1000;
 int stepperSpeed = 300;
@@ -28,6 +43,7 @@ int count2 = 0;
 char * val;
 
 void setup() {
+  // Set up the motors
   Serial.begin(9600);
   stepper1.setMaxSpeed(maxSpeed);
   stepper1.setAcceleration(acceleration);
@@ -37,24 +53,31 @@ void setup() {
   stepper2.setAcceleration(acceleration);
   stepper2.setSpeed(stepperSpeed);
   
+  // zero out where all out motors are starting
   stepper1.moveTo(INCH);
   stepper2.moveTo(INCH);
 }
 
 void loop() {
+
+  // if we're getting data from the processing code,
+  // read in the data
   if (Serial.available()) {
     val = Serial.read();
-  
+    // parse the values at the commas, store in array
     char * dist = strtok(val, ",");
   
   //if (stepper1.distanceToGo() == 0) {
+    // calculate how far to move
     int moving = calcDist(float(dist[0]));
+    move that many inches (since moving is in inches)
     stepper1.moveTo(moving*INCH);
+    // testing purposes
     Serial.print("HI HI HI ");
     Serial.println(String(dist[0]));
   //}
   
-  
+  // Do the same process as above for the second motor
   //if (stepper1.distanceToGo() == 0) {
     int moving2 = calcDist(float(dist[1]));
     stepper2.moveTo(moving2*INCH);
@@ -62,10 +85,12 @@ void loop() {
     Serial.println(String(dist[1]));
   //}
   }
+  // run the motors
   bool step1 = stepper1.run();
   bool step2 = stepper2.run();
 }
-
+ // function to convert number coming into processing
+ // into a number we want to use
 int calcDist(float num) {
   float distance = ROTATE*num;
   return (int)distance;
